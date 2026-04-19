604467b97cb3bf0a1d015b2b3c50a4da3e188a5f
martta <martineidger@gmail.com>
2026-04-19 17:40:46 +0300

    add build files and switch from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { BookingCalendar } from './components/BookingCalendar';
@@ -10,18 +10,18 @@ import { HomePage } from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/carwash/" element={<CarwashPage />} />
-        <Route path="/detailing/" element={<DetailingPage />} />
-        <Route path="/price/" element={<PricePage />} />
-        <Route path="/contacts/" element={<ContactsPage />} />
+        <Route path="/carwash" element={<CarwashPage />} />
+        <Route path="/detailing" element={<DetailingPage />} />
+        <Route path="/price" element={<PricePage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
       </Routes>
       <Footer />
       <BookingCalendar />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
